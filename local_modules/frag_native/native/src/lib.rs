f8fe3bf95c1767d17609e4a393fca606a0d4244c
Paul Miller <paul@pauljmiller.com>
2018-12-07 13:37:33 -0800

    I put some functions in a different file and it didn't break!

--- local_modules/frag_native/native/src/lib.rs
+++ local_modules/frag_native/native/src/lib.rs
@@ -1,29 +1,14 @@
 #[macro_use]
 extern crate neon;
 
-use grep::regex::RegexMatcher;
-use grep::searcher::sinks::UTF8;
-use grep::searcher::{BinaryDetection, SearcherBuilder};
-
 use std::fs::File;
 use std::io::prelude::*;
-use std::time::{Duration, Instant};
 
 use std::ffi::OsString;
 
-use walkdir::{DirEntry, WalkDir};
-
 use neon::prelude::*;
 
-use std::error::Error;
-
-fn is_hidden(entry: &DirEntry) -> bool {
-  entry
-    .file_name()
-    .to_str()
-    .map(|s| s.starts_with('.'))
-    .unwrap_or(false)
-}
+mod search;
 
 //builds were failing on linux so we found this workaround
 //https://users.rust-lang.org/t/neon-electron-undefined-symbol-cxa-pure-virtual/21223/2
@@ -32,77 +17,11 @@ pub extern "C" fn __cxa_pure_virtual() {
   loop {}
 }
 
-struct ListItem {
-  pub path: String,
-  pub file_name: String,
-  pub line: String,
-  pub line_num: u64,
-}
-
-fn grep_life(pattern: &str) -> Result<Vec<ListItem>, Box<Error>> {
-  let mut matches: Vec<ListItem> = vec![];
-  let matcher = RegexMatcher::new(&pattern)?;
-  let dir = OsString::from("./node_modules/");
-  // let dir = OsString::from("../notes_grep_test/");
-  let mut searcher = SearcherBuilder::new()
-    .binary_detection(BinaryDetection::quit(b'\x00'))
-    .build();
-
-  //hack to keep search times low. we give up after 10ms
-  let start_time = Instant::now();
-  let timeout_duration = Duration::from_millis(10);
-  let end_time = start_time + timeout_duration;
-
-  for result in WalkDir::new(dir)
-    .into_iter()
-    .filter_entry(|e| !is_hidden(e))
-    .take_while(|_| Instant::now() < end_time)
-  {
-    let dent = match result {
-      Ok(dent) => dent,
-      Err(err) => {
-        eprintln!("{}", err);
-        continue;
-      }
-    };
-    if !dent.file_type().is_file() {
-      continue;
-    }
-    let result = searcher.search_path(
-      &matcher,
-      dent.path(),
-      UTF8(|lnum, line| {
-        //TODO: this is a hacky way to convert the path to the string
-        // and get rid of the extra quotation marks that :? adds
-        let path = dent.path().display();
-        // let trimmed_path = path.trim_start_matches('"').trim_end_matches('"');
-        matches.push(ListItem {
-          path: path.to_string(),
-          file_name: dent.file_name().to_os_string().into_string().unwrap(),
-          line: line.to_string(),
-          line_num: lnum,
-        });
-        Ok(true)
-      }),
-    );
-    if let Err(err) = result {
-      eprintln!("{}: {}", dent.path().display(), err);
-    }
-    //TODO: convert this to a "take" and somehow add more as
-    //the user scrolls
-    if matches.len() >= 10 {
-      break;
-    }
-  }
-
-  Ok(matches)
-}
-
 fn query(mut cx: FunctionContext) -> JsResult<JsArray> {
   let query = cx.argument::<JsString>(0)?.value();
 
   //TODO: fix this unwrap
-  let vec = grep_life(&query).unwrap();
+  let vec = search::grep_life(&query).unwrap();
 
   // Create the JS array
   let js_array = JsArray::new(&mut cx, vec.len() as u32);
